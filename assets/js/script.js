29d53f3a6cebe12f71a04a0b4f0b16dbd681e5cc
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-07 15:13:04 +0200

    refactoring for cicle to show images with foreach

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,7 @@
 /* 
 
-Consegna:
-Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
-
-
-MILESTONE 1
-Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
-
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
-MILESTONE 3
-Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-
-
-BONUS 1:
-Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-
-
-BONUS 2:
-Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato. 
-Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
-
-Prima di partire a scrivere codice:
-Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
-
-Consigli del giorno:
-1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template". 
-2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
-3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
-
-Buon lavoro e buon divertimento! :slightly_smiling_face:
-
-
-
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-
-
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
-<img class="active" src="./assets/img/01.webp" alt="">
-*/
-
-
 /* Define the slides list */
-const slides = [
+const slides2 = [
     './assets/img/01.webp', //0
     './assets/img/02.webp', //1
     './assets/img/03.webp', //etc
@@ -61,19 +9,49 @@ const slides = [
     './assets/img/05.webp',
   ]
   
-  let activeSlide = 0;
+const slides = [
+    {
+        position : 1,
+        url : './assets/img/01.webp',
+        title : 'spiderman'
+    },
+    {
+        position : 2,
+        url :'./assets/img/02.webp',
+        title : 'topini'
+    },
+    {
+        position : 3,
+        url :'./assets/img/03.webp',
+        title : 'vari personaggi'
+    },
+    {
+        position : 4,
+        url :'./assets/img/04.webp',
+        title : 'un gatto'
+    },
+    {
+        position : 5,
+        url :'./assets/img/05.webp',
+        title : 'the avengers'
+    },
+]
+
+  let activeSlide = 1;
   
   // select the dom elements
   const sliderImagesEl = document.querySelector('.slider .images')
   const prevEl = document.querySelector('.prev')
   const nextEl = document.querySelector('.next')
+const titleEl = document.querySelector('imageTitle')
   
   
   //console.log(sliderImagesEl);
   
   /* Print all images into the dom */
   // loop over the slides 
-  for (let i = 0; i < slides.length; i++) {
+
+  /* for (let i = 0; i < slides2.length; i++) {
     const slidePath = slides[i];
     console.log(slidePath);
     
@@ -83,8 +61,22 @@ const slides = [
   
     sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
   
-  }
+  } */
   
+  slides.forEach(slide => {
+
+    //const imageURL = slide.url
+
+    const slideMarkup = `<img class="${activeSlide === slide.position ? 'active' : '' }" src="${slide.url}" alt="">`
+    //console.log(slideMarkup);
+
+    //const slideMarkup = `<img src="${slide.url}" alt="">`
+  
+    sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
+
+    console.log(slide.url);
+    
+  });
   
   /* 
   
@@ -131,14 +123,24 @@ const slides = [
   
   const thumbsElement = document.querySelector('.thumbnails')
   
-  for (let i = 0; i < slides.length; i++) {
+  /* for (let i = 0; i < slides.length; i++) {
     const thumbPath = slides[i];
     const thumbMarkup = `<img class="thumb ${activeSlide === i ? 'active' : ''}" src="${thumbPath}" alt="">`
     //console.log(thumbMarkup);
   
     thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
     
-  }
+  } */
+
+  slides.forEach(slide => {
+
+    const thumbPath = slide.position;
+    const thumbMarkup = `<img class="thumb ${activeSlide === thumbPath ? 'active' : ''}" src="${slide.url}" alt="">`
+    //console.log(thumbMarkup);
+  
+    thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
+    
+  });
   
   
   
